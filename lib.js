08903c864738810b0b32813809ed31a07bf93b25
altairObandoAxxis <101345036+altairObandoAxxis@users.noreply.github.com>
2023-08-22 12:39:01 +0000

    Added login and store token in cache

--- lib.js
+++ lib.js
@@ -1,13 +1,29 @@
 const axios = require('axios');
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
 require('dotenv').config();
 
 const doCmd = async ({ cmd, data }) =>{
+    const token = cache.get('Token') || await getToken();
     try {
-        const response = await axios.post(process.env.API_URL, { cmd, data },{ headers: { 'Authorization': `Bearer ${ process.env.API_TOKEN }` }});
+        const response = await axios.post(process.env.API_URL, { cmd, data },{ headers: { 'Authorization': `Bearer ${ token }` }});
         global[cmd] = response.data;
         return response.data
     } catch (error) {
         global[cmd] = { cmd, outData: null , msg: error }
+    }
+}
+
+const getToken = async()=>{
+    try{
+        const response = await axios.post(process.env.API_LOGIN,{ email: process.env.API_USER, clave: process.env.API_PASS});
+        const { ok, msg, outData: { token }} = response.data;
+        if(!ok)
+            throw msg;
+        cache.set('Token', token);
+        return token
+    }catch(error){
+        throw error
     }
 }
 
